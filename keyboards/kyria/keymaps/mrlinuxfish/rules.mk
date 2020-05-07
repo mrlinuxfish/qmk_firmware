6b865c498214698e25eb2f47d2d1f22f719cb69a
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2020-05-06 21:53:01 -0600

    Initial commit

---
+++ keyboards/kyria/keymaps/mrlinuxfish/rules.mk
@@ -0,0 +1,7 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # ENables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+MOUSEKEY_ENABLE = yes 	   # Mouse keys
+EXTRAKEY_ENABLE = yes      # Audio control and System control
+NKRO_ENABLE = yes          # N-key rollover
+BOOTMAGIC_ENABLE = lite	   # Bootmagic lite (only reset)
