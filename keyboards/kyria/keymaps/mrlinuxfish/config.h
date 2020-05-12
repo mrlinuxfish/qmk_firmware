f39022a6314914a9ade635a9fc77d47be756fa5e
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2020-05-12 16:18:51 -0600

    Update settings for homerow mods The previous settings did not work correctly at high typing speeds

--- keyboards/kyria/keymaps/mrlinuxfish/config.h
+++ keyboards/kyria/keymaps/mrlinuxfish/config.h
@@ -16,10 +16,10 @@
 
 #pragma once
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
+// Fix problems with fast typing and homerow mods
+#define TAPPING_TERM 150
+#define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
-
-// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
 
 // Recommended for heavy chording.
