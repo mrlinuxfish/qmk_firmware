48fd19677fdeb83cd150547ca46af443be1a8907
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2020-06-18 16:01:29 -0600

    Fix encoder resolution

--- keyboards/kyria/keymaps/mrlinuxfish/config.h
+++ keyboards/kyria/keymaps/mrlinuxfish/config.h
@@ -49,3 +49,8 @@
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
 #define NO_ACTION_ONESHOT
+
+// EC11K encoders have a different resolution than other EC11 encoders.
+// When using the default resolution of 4, if you notice your encoder skipping
+// every other tick, lower the resolution to 2.
+#define ENCODER_RESOLUTION 2
