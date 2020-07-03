61c952b9f14b001577eb4a836eef665318fa1538
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2020-07-03 15:52:44 -0600

    Change animation values

--- keyboards/kyria/keymaps/mrlinuxfish/keymap.c
+++ keyboards/kyria/keymaps/mrlinuxfish/keymap.c
@@ -180,12 +180,12 @@ uint16_t get_permissive_hold(uint16_t keycode, keyrecord_t *record) { // {{{
 #ifdef OLED_DRIVER_ENABLE
 // WPM-responsive animation stuff here
 #define IDLE_FRAMES 5
-#define IDLE_SPEED 40 // below this wpm value your animation will idle
+#define IDLE_SPEED 30 // below this wpm value your animation will idle
 
 // #define PREP_FRAMES 1 // uncomment if >1
 
 #define TAP_FRAMES 2
-#define TAP_SPEED 60 // above this wpm value typing animation to trigger
+#define TAP_SPEED 45 // above this wpm value typing animation to trigger
 
 #define ANIM_FRAME_DURATION 200 // how long each frame lasts in ms
 // #define SLEEP_TIMER 60000 // should sleep after this period of 0 wpm, needs fixing
