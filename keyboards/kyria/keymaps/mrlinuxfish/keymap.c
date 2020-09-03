b472208b92e9c7097e8c59673e8c95d0e3672c8f
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2020-09-03 12:12:23 -0600

    Fix the overflow again - The buffer needs room a null terminator character at the end in addition to the 9 characters in the string

--- keyboards/kyria/keymaps/mrlinuxfish/keymap.c
+++ keyboards/kyria/keymaps/mrlinuxfish/keymap.c
@@ -378,7 +378,7 @@ static void render_status(void) {
     oled_write_P(IS_LED_ON(led_usb_state, USB_LED_SCROLL_LOCK) ? PSTR("SCRLCK ") : PSTR("       "), false);
 }
 
-char wpm_str[9];
+char wpm_str[10];
 void oled_task_user(void) {
     if (is_keyboard_master()) {
         render_status();
