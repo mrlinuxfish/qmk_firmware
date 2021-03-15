91cff7cd329caa1bad6b7529f8dac7cd9088d148
Dustin Bosveld <steamyleafjuice@protonmail.com>
2021-03-15 14:51:50 -0600

    Fix qmk-heatmap

--- keyboards/minidox/keymaps/mrlinuxfish/keymap.c
+++ keyboards/minidox/keymaps/mrlinuxfish/keymap.c
@@ -443,8 +443,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             uprintf("0x%04X,%u,%u\n", keycode, record->event.key.row, record->event.key.col);
         }
     #endif
-    switch (keycode) {
-    ...
-    }
     return true;
 }
