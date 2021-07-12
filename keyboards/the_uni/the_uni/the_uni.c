86545992c72940e15a019ec687df8e755e26623a
Dustin Bosveld <steamyleafjuice@protonmail.com>
2021-07-12 17:56:50 -0600

    Add the_uni files from Uni git repo

--- keyboards/the_uni/the_uni/the_uni.c
+++ keyboards/the_uni/the_uni/the_uni.c
@@ -15,17 +15,4 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#pragma once
-#define THE_UNI_H
-
-#include "quantum.h"
-
-#define LAYOUT( \
-	K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, \
-	K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, \
-	            K202, K203, K204, K205, K206, K207  \
-) { \
-	{ K000,  K001,  K002,  K003,  K004,  K005,  K006,  K007,  K008,  K009,  K010 }, \
-	{ K100,  K101,  K102,  K103,  K104,  K105,  K106,  K107,  K108,  K109,  K110 }, \
-	{ KC_NO, KC_NO, K202,  K203,  K204,  K205,  K206,  K207,  KC_NO, KC_NO, KC_NO }  \
-}
+#include "the_uni.h"
